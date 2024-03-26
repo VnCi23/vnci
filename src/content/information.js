5cd7308509714e15977cc16a8850914a4d3e810c
Your Name <vincegaurino@gmail.com>
2024-03-26 21:11:22 +0800

    i

--- src/content/information.js
+++ src/content/information.js
@@ -4,7 +4,7 @@ const userData = {
   firstName: "<Vn",
   lastName: "Ci/>",
   title: "About Me",
-  img: "https://scontent.fmnl2-2.fna.fbcdn.net/v/t39.30808-1/399017635_1325513508355521_5227546487647033299_n.jpg?stp=dst-jpg_p320x320&_nc_cat=108&cb=99be929b-b574a898&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFvflYTpcWE81njJre7Kc5-DOUGlQ4ifncM5QaVDiJ-dwzTwgwgZOvI3QH9Mpb0etWDusUYWWhgDG-bOPK6NVI1&_nc_ohc=Oso93SBrO9AAX-KARWO&_nc_ht=scontent.fmnl2-2.fna&oh=00_AfDY4WVVTQmFJnrB61FTuow9YFjb-Agh1ZZDxCfr40kmfg&oe=65EEE111",
+  img: "https://github.com/VnCi23/files/blob/main/New%20folder/0.png?raw=true",
   description:
     "I’m a web dev enthusiast from the Philippines. I love making cool and user-friendly websites. Always learning new stuff to keep my sites top-notch. ",
 };
